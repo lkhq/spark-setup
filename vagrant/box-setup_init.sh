26951fcd5b29866611b9759c8b036eeb086485d4
Matthias Klumpp <matthias@tenstral.net>
2018-02-15 04:48:17 +0100

    Use eatmydata for some speed gain
    This is used for testing instances anyway, so using eatmydata is fine.

--- vagrant/box-setup_init.sh
+++ vagrant/box-setup_init.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+apt-get install --force-yes -fuy eatmydata
+
 # update to buster (if not on buster already)
 printf "\ndeb http://deb.debian.org/debian/ buster main\n" > /etc/apt/sources.list
 
@@ -12,5 +14,5 @@ apt-get update
 apt-get purge mysql-common
 apt-get purge exim4
 
-apt-get -o Dpkg::Options::='--force-confnew' --force-yes -fuy dist-upgrade
-apt-get --purge autoremove
+eatmydata apt-get -o Dpkg::Options::='--force-confnew' --force-yes -fuy dist-upgrade
+eatmydata apt-get --purge -fuy autoremove
